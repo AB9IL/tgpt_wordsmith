81936645e8a9cf33e20b8827dfedb5316d007ce3
Philip Collier <redcrown01@gmail.com>
2025-08-10 11:55:34 +0000

    My comments about the latest changes.

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 # tgpt_wordsmith
+
 Scripts for using tgpt to create outline based web or book content using AI.
 
 #### Description
 
 These scripts are written in Bash. They read files containing data for building prompts, then run Terminal-GPT (tgpt) to create html or plaintext for use in web pages, books, or other prose.
+
+Since the script builds the output file with many sequential queries to the AI service, you can probe topics in depth and produce multiple thousands of words of output.
 
 These are a work in progress...
 
@@ -14,5 +17,6 @@ These are a work in progress...
 
 #### Suggestion:
 
-- Know or have access to accurate knowledge about the topics. You need to create prompts focused on pertinent information in order to create good content. The AI models do not have judgement and will say dumbass things, which you must find and correct in order to keep your quality.
+- Know or have access to accurate knowledge about the topics. You need to create prompts focused on pertinent information in order to create good content. The AI models do not have judgement and will say silly or incorrect things, which you must find and correct in order to maintain your quality.
+- The examples build multiple chapters or sets of web pages with raw data for prompts in book/chapter and group/misc directories.
 - Be sure to edit the output and remove any junk text from the AI session.
--- ai-page-creation/pagemaker-hijkl.sh
+++ ai-page-creation/pagemaker-hijkl.sh
@@ -7,31 +7,55 @@
 # (at your option) any later version. There is NO warranty; not even for
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 
-# This script reads topical information, creates a prompt and generates
-# html suitable for insertion in live web pages. It uses golang based
-# tgpt.
+# This script reads topical information and several subtoical items to
+# create a prompt and generate html suitable for insertion in live web
+# pages. It uses golang based tgpt.
 
-PROV_NAME="pollinations"
-OUTPUTPATH="output"
-TOPICS_FILE="hijkl-misc/topics" # broader subject to discuss
-ITEMS_FILE="hijkl-misc/items"   # more specific topic
-TOP_DATA="$(<misc/hijkl-misc/top)"
-BOTTOM_DATA="$(<misc/hijkl-misc/bottom)"
+# This script is intended for creation of deep, thousand word pages, each
+# on a specific topic but covering the "items"" in relation to the various
+# main topics.
+#
+#  For example, a list of "pizza restaurants" for topics and a list of
+#  pizza aspects for coverage on each page: pepperoni, supreme, corn-and-peppers,
+#  and so forth.
+#
+#  To round out a website, you should prepare dozens of pairs of topic and items
+#  files!
+
+SITE_CODE="hijkl"
+PROV_NAME="phind"
+PROMPT_SOURCES=(misc/"$SITE_CODE"-misc/group*)
+OUTPUTPATH="finished-pages/$SITE_CODE"
+TOPICS_FILE="topics"                          # broader subject to discuss
+ITEMS_FILE="items"                            # more specific topics
+TOP_DATA="$(<misc/$SITE_CODE-misc/top)"       # generic top of page
+BOTTOM_DATA="$(<misc/$SITE_CODE-misc/bottom)" # generic bottom of page
 export TGPT="/usr/local/src/gotgpt/tgpt-linux-amd64"
 
-IFS=$'\n' read -rd '' -a ITEMS <<<"$(\cat "$ITEMS_FILE")"
-IFS=$'\n' read -rd '' -a TOPICS <<<"$(\cat "$TOPICS_FILE")"
+# create an output directory if it does not exist
+[ -d "$OUTPUTPATH" ] || mkdir $OUTPUTPATH
 
-for K in "${TOPICS[@]}"; do
-    for Q in "${ITEMS[@]}"; do
-        TIME="$(date +"%Y-%m-%d_%H-%M-%S")"
-        echo "Working on $K and $Q"
-        PROMPT="You will create HTML body text for an existing page; do not
-        create any <head> or <h1> elements. Find recent information about $K with
-        emphasis and details on $Q, and create HTML formatted text. Start your text with
-        <h2>$Q Timeline of Gaffes</h3>."
+for SOURCE in "${PROMPT_SOURCES[@]}"; do
+    # read topics and items
+    IFS=$'\n' read -rd '' -a TOPICS <<<"$(\cat "$SOURCE"/"$TOPICS_FILE")"
+    IFS=$'\n' read -rd '' -a ITEMS <<<"$(\cat "$SOURCE"/"$ITEMS_FILE")"
+    S=1 # sequence number
+    for K in "${TOPICS[@]}"; do
+        TIME="$(date +"%Y-%m-%d_%H-%M-%S")" #timestamp
+        echo -e "\n$TOP_DATA" >"${OUTPUTPATH}/${TIME}-${S}.html"
+        for Q in "${ITEMS[@]}"; do
+            echo "Working on $K and $Q"
+            PROMPT="You will create HTML body text for an existing page; do not
+            create any <head> or <h1> elements. Find recent information about $K with
+            emphasis and details on $Q, and create HTML formatted text. Follow a narrative
+            (storytelling) style. Use <h3> headers, bold, and italic text for emphasis,
+            where appropriate. Avoid emdashes and bullet
+            points."
 
-        CONTENT="$($TGPT --provider "${PROV_NAME}" "${PROMPT}")"
-        echo -e "$TOP_DATA\n$CONTENT\n$BOTTOM_DATA" >"${OUTPUTPATH}/${TIME}.html"
+            CONTENT="$($TGPT --provider "${PROV_NAME}" "${PROMPT}")"
+            echo -e "\n$CONTENT\n" >>"${OUTPUTPATH}/${TIME}-${S}.html"
+        done
+        echo -e "\n$BOTTOM_DATA" >>"${OUTPUTPATH}/${TIME}-${S}.html"
+        ((S++))
     done
 done
